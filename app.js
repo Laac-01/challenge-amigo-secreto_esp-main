7707cbafbc486d07f6da0a19e5971f5364357758
Laac_0_0 <armandoluiscas@gmail.com>
2025-01-18 23:10:53 -0600

    creacion-de-la-funcion-agregar-amigo

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//variables iniciales
+let arregloDeAmigos = []; //arreglo donde se almacenaran los nombres de mis amigos.
+
+
+//cracion de la funcion agregar un amigo 
+function agragarUnAmigo(){
+
+   let nombre = document.getElementById("amigo").value;//obtengo le valor de la caja de texto
+   if(nombre != ''){ //si nombre no es '' entonces 
+        arregloDeAmigos.push(nombre);//agrego el nombre a el arreglo 
+        document.getElementById('amigo').value = '';//limpio la caja de texto
+        //console.log(arregloDeAmigos); //imprime los nombres por consola
+   }else{
+        alert('Ingrese un nombre valido');
+   }
+
+}
+
+console.log(`La lista de amigos es ${arregloDeAmigos}`);
+
