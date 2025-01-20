ce97a2a87a1f382f12f2c0c72c306da6a81a743d
Laac_0_0 <armandoluiscas@gmail.com>
2025-01-19 18:03:40 -0600

    se-agregaron-las-funciones-sorteoAmigo-listarAmigo

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ Juego amigo secreto
 
 Funcionalaidades:
 
+    1. `agregarAmigo()`
+        -Está funcion toma los parametros de la caja de texto y los gusrda en un arreglo.
+    2.`sorteoAmigo()`
+        -Esta funcion realiza el sorteo del amigo secreto mediante en sorteo aleatorio, tomando el tamaño del arreglo arregloAmogos y multiplicando esto por la funcon Math.ramdon()
+    3.`listarAmigos()`
+        -Está funcion imprime en pantalla los nombres que se van agreagando.
+
--- app.js
+++ app.js
@@ -10,11 +10,49 @@ function agragarUnAmigo(){
         arregloDeAmigos.push(nombre);//agrego el nombre a el arreglo 
         document.getElementById('amigo').value = '';//limpio la caja de texto
         //console.log(arregloDeAmigos); //imprime los nombres por consola
+        listarAmigos();
    }else{
         alert('Ingrese un nombre valido');
    }
 
 }
 
-console.log(`La lista de amigos es ${arregloDeAmigos}`);
+ //se obtiene el amigo secreto con un numero aleatorio
+function sorteoDeAmigo(){
+
+     if(arregloDeAmigos.length ==  0){
+          alert('Ingresa un amigo...');
+          return;//termina la funcion.
+     }else{
+
+          let numeroAleatorio = Math.floor(Math.random() * arregloDeAmigos.length);//el numero se calcula con el tamañano del arreglo, en este caso seria la cantidad de amigos introducido y el numero va de cero a arregleDeAmigos.length
+          let amigoSecreto = arregloDeAmigos[numeroAleatorio];
+     
+          let resusltado = document.getElementById('resultado');
+          resusltado.innerHTML = '';
+          let rec = document.createElement('rec');
+          rec.textContent = `Tu amigo secreto es ${amigoSecreto}`//se asigna el amigo secreto
+          resusltado.appendChild(rec);
+          
+          //limpiamos la lista 
+          let lista = document.getElementById('listaAmigos');
+          lista.innerHTML = '';//limpiamos la pantalla para que solo se muetre el amigo secreto.
+     }
+}
+
+
+// Función para listar los amigos en la página
+function listarAmigos() {
+
+     let lista = document.getElementById('listaAmigos');
+     lista.innerHTML = ''; // Limpiar la lista antes de agregar los nuevos nombres
+ 
+     // Recorrer el arreglo de amigos y agregar cada uno a la lista
+     for (let i = 0; i < arregloDeAmigos.length; i++) {
+         let li = document.createElement('li'); 
+         li.textContent = arregloDeAmigos[i];//se agregan los amigos para su imprecion
+         lista.appendChild(li); 
+     }
+ }
+
 
